a53d85f1c7b19d754c9a13c7b3fdd65ae0e9ab70
leomet07 <leomet07@gmail.com>
2021-12-01 20:06:36 -0500

    1539C debugging...

---
+++ 1539C/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+bool isStable(vector<ll> s, ll x){
+    for (int i = 1; i < s.size(); i++){
+        if (s[i] - s[i - 1] > x){
+            return false;
+        }
+    }
+    return true;
+}
+
+void printV(vector<ll> v){
+    cout << "Printing array: " << endl;
+    for (auto i : v) {
+        cout <<  i << endl;
+    }
+    cout << "End" << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+
+    ll n, k, x;
+    cin >> n >> k >> x;
+
+    vector<ll> levels;
+    for (ll i = 0; i < n; i++){
+        ll l;
+        cin >> l;
+        levels.push_back(l);
+    }
+
+    sort(levels.begin(), levels.end());
+
+    printV(levels);
+
+    bool run = !isStable(levels, x);
+
+    ll b = 1;
+    if (run){
+
+        for (int i = 1; i < levels.size(); i++){
+            ll dist = levels[i] - levels[i - 1];
+            cout << "Distance: " << dist << " " << dist % x << endl;
+            if ( dist >= x){
+                if (dist % x >= k){
+                    cout<< "Fail sad" << endl;
+                    // fail, so we must break
+                    b++;
+                }
+            }
+        }
+    } 
+    // else{
+    //     cout << "Not runnning? " << endl;
+    // }
+
+    cout << b << endl;
+    return 0;
+
+}
