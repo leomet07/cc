af084587e2912cfc34ae8e23e5f5f56635d451a0
leomet07 <leomet07@gmail.com>
2021-12-18 11:11:57 -0500

    Basic slow lonley cows func

---
+++ lonelycows/main.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	ifstream cin("l.in");
+
+	ll n;
+	cin >> n;
+
+	string f;
+	cin >> f;
+
+	char cows[n];
+	for (ll i = 0; i < f.length(); i++){
+		char c = f.at(i);
+		cows[i] = c;
+	}
+
+	for (ll i = 0; i < n; i++){
+		cout << "in cows: " << cows[i] << endl;
+	}
+
+	// for (ll seqlen = 3; seqlen < n + 1; seqlen++ ){
+	ll lonlies = 0;
+	for (ll seqlen = 3; seqlen < 4; seqlen++ ){
+		
+		cout << "Possible seqlen: " << seqlen << endl;
+		// count cows
+		
+		for (ll left = 0; left < n - (seqlen -1); left++){
+			ll h = 0;
+			ll g = 0;
+			ll right = left + seqlen;
+			cout << "Left: " << left << " Right: " << right << endl;
+
+
+			// count cows
+			for (ll i = left; i < right; i++){
+				char t = cows[i];
+				cout << "Cow in looper: " << t << endl;
+				if(t == 'H'){
+					h++;
+				}else if (t == 'G'){
+					g++;
+				}
+			}
+
+			cout << "G cows counted: " << g << " H cows counted: " << h << endl;
+
+			if (g == 1 || h == 1){
+				cout << "Lonley possbility found, so incrementing lonlies" << endl;
+				lonlies++;
+			}
+		}
+	}
+
+	cout << "Lonlies: " << lonlies << endl;
+
+	return 0;
+}
